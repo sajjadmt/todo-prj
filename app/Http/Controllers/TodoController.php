9ce253f2b808d756338f16be4ef5cb7f1ebc4c96
Sajjad Momenitabar <momenitabar@gmail.com>
2023-08-05 11:02:56 +0330

    add index function to TodoController.php

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    //
+
+    public function index()
+    {
+        $todos = Todo::latest()->paginate(5);
+
+        return view('todos.index' , compact('todos'));
+    }
+
 }
