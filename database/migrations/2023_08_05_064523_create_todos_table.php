4115700ec44c01759b5c728215303b48668f541a
Sajjad Momenitabar <momenitabar@gmail.com>
2023-08-05 10:22:12 +0330

    complete todo migration

--- database/migrations/2023_08_05_064523_create_todos_table.php
+++ database/migrations/2023_08_05_064523_create_todos_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('title');
+            $table->text('body');
+            $table->boolean('completed')->default(0);
             $table->timestamps();
         });
     }
