84b0d5eeca881b40e831f4b16a6349d14026f815
Sajjad Momenitabar <momenitabar@gmail.com>
2023-08-05 10:14:37 +0330

    Add user_type field to user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('user_type')->default(\App\Enums\UserType::User->value);
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
