5da16ad333afd8513c6c0e153e9015bab01dfcc1
Sajjad Momenitabar <momenitabar@gmail.com>
2023-08-05 10:45:32 +0330

    complete index.blade.php html

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-8">
+                <h4>Tasks</h4>
+                <div class="card">
+                    <div class="card-header">
+                        Tasks
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item">
+                                Task1
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
